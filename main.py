c3ae2350f890bf6eab44bd028b05319e2d55dda1
Nitroxt5 <filippov.maxim52@gmail.com>
2025-08-12 17:02:12 +0300

    Icon added

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
+
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication
 
 from ui.widgets.main_widget import MainWindow
@@ -6,6 +8,7 @@ from ui.widgets.main_widget import MainWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon('media\\icon.ico'))
     window = MainWindow(app.primaryScreen().availableGeometry())
     window.show()
     sys.exit(app.exec_())
